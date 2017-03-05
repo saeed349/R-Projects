d697f1888127788e8825b3e4af36943bc48567cb
Saeed Rahman <saeed349@gmail.com>
2017-03-05 17:30:23 -0500

    implemented binomial tree stock process and option the way clelaw did for trinomial

--- 621/Assignment2/Question1.R
+++ 621/Assignment2/Question1.R
@@ -21,30 +21,6 @@ BinomialTree= function(isCall, isAmerican, K=100, Tm=1,
     }
   }
   S
-  
-  print("--------")
-  step=3
-  m=(step-1)*2+3
-  n=step+1
-  u=1.1
-  d=1/u
-  s.tree =matrix(NA,nrow=m,ncol=n,dimnames=list(
-    paste("State", 1:(m),sep=""), paste("T=",0:step,sep="")))
-  s.tree
-  s.tree[step+1,1]=100
-  s.tree
-  for(j in 2:n){
-    for(i in 1:m){
-      if(i+1<=m)
-        if(!is.na(s.tree[i+1,j-1]))
-          s.tree[i,j]=s.tree[i+1,j-1]*u
-      if(i-1>=1)
-        if(!is.na(s.tree[i-1,j-1]))
-          s.tree[i,j]=s.tree[i-1,j-1]*d
-    }
-  }
-  print(s.tree)
-  
   # Intialize option values at maturity ----
   for (j in 1:M) {
     V[M-j+1, M] = max( 0, cp * (S[M-j+1, M]-K))
@@ -73,9 +49,6 @@ Clewlow3_3 = function(isCall, isAmerican, K=100, Tm=1,
   # Precompute constants ----
   dt = Tm/N 
   nu = r - div - 0.5 * sig^2
-  pu = 0.5 * ( (sig^2*dt + nu^2 *dt^2)/dx^2 + nu*dt/dx )
-  pm = 1.0 -   (sig^2*dt + nu^2 *dt^2)/dx^2 #### sideways market
-  pd = 0.5 * ( (sig^2*dt + nu^2 *dt^2)/dx^2 - nu*dt/dx )
   disc = exp(-r*dt)
   u=1.1
   d=1/u
@@ -89,16 +62,46 @@ Clewlow3_3 = function(isCall, isAmerican, K=100, Tm=1,
   S[nCols, 1] = S0
   for (j in 1:N) {
     for(i in (nCols-j+1):(nCols+j-1)) {
+      print((nCols-j+1):(nCols+j-1))
+      print(paste("i=",i,"j=",j))
       S[i-1, j+1] = S[i, j] * u
       S[i+1, j+1] = S[i, j] * d
+      print("---------")
     }
   }
+  # $S
   print(S)
+  M=N+1
+  for (i in 1:nRows) {
+    V[i, M] = max( 0, cp * (S[i, M]-K))
+  }
+  print(V)
+  # V
+  # Step backwards through the tree ----
+  p = (exp(r*dt)-d)/(u-d)
+  for (j in (nCols-1):1) {
+    for(i in (nCols-j+1):(nCols+j-1)) {
+      V[i, j] = disc * (p*V[i-1,j+1] + (1-p)*V[i+1,j+1])
+      if(isAmerican) {
+        V[i, j] = max(V[i, j], cp * (S[i, j] - K))
+      }
+    } 
+  }
+  V
 }
+
+# for (j in (M-1):1) {
+#   for (i in 1:j) {
+#     V[i, j] = disc * ( p*V[i, j+1] + (1-p)*V[i+1, j+1] )
+#     if(isAmerican) {
+#       V[i, j] = max(V[i, j], cp * (S[i, j] - K))
+#     }
+#   }
+# }
 
 Clewlow3_3(isCall=T, isAmerican=F)
 
-# BinomialTree(isCall=T, isAmerican=F)
+BinomialTree(isCall=T, isAmerican=F)
 # Clewlow2_3n5(isCall=F, isAmerican=T)
 
 
