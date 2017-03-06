ee2859c2cc51578e24dddb6612afb8207f8a1b61
Saeed Rahman <saeed349@gmail.com>
2017-03-06 11:30:17 -0500

    got so messed up with secant, the denominator in secant method is going to zero

--- 621/Assignment2/Question1.R
+++ 621/Assignment2/Question1.R
@@ -1,4 +1,5 @@
-BinomialTree= function(isCall, isAmerican, K=100, Tm=1, 
+#professors implementation
+BinomialTreeOld= function(isCall, isAmerican, K=100, Tm=1, 
                         S0=100, r=0.06, N=3, u=1.1, d=1/u)
 {
   # Precompute constants ----
@@ -42,57 +43,59 @@ BinomialTree= function(isCall, isAmerican, K=100, Tm=1,
        Price = V[1,1], S=round(S,2), V=round(V,4))
 }
 
-Clewlow3_3 = function(isCall, isAmerican, K=100, Tm=1, 
-                      S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2)
+#inspired from clewlaws trinomial tree
+BinomialTree = function(isCall, isAmerican, K=100, Tm=1, 
+                      S0=100, r=0.06, sig=0.2, N=3, u=1.1, d=1/u)
 {
-  # Trinomial Tree: j times, 2*j+1 final nodes
   # Precompute constants ----
-  dt = Tm/N 
-  nu = r - div - 0.5 * sig^2
+  dt = Tm/N # diving the time into partition, to get time for each state
   disc = exp(-r*dt)
-  u=1.1
-  d=1/u
-  nRows = 2*N+1
-  nCols = N+1
-  cp = ifelse(isCall, 1, -1)
+  p = (exp(r*dt)-d)/(u-d) #calculating the probability of up move
+  nRows = 2*N+1 #number of rows for the matrix
+  nCols = N+1 #number of columns
+  cp = ifelse(isCall, 1, -1) # to check ifts a call or a put
   
   # Intialize asset prices  ----
+  # Creating a matrix of nRows*nColumns with zeros and headings 
   V = S = matrix(0, nrow=nRows, ncol=nCols, dimnames=list(
     paste("NumUps", N:-N, sep="="), paste("T", 0:3, sep="=")))
-  S[nCols, 1] = S0
+  S[nCols, 1] = S0 # initial stock price
+  
+  # iterating the elements of the matrix in a conical manner starting
+  # from the position of initial stock price
+  # For n=3, S[i,j]= S0, then update the forward diagonal elements 
+  # Code is similar to the one used for trinomial tree
   for (j in 1:N) {
     for(i in (nCols-j+1):(nCols+j-1)) {
-      print((nCols-j+1):(nCols+j-1))
-      print(paste("i=",i,"j=",j))
       S[i-1, j+1] = S[i, j] * u
       S[i+1, j+1] = S[i, j] * d
-      print("---------")
     }
   }
-  # $S
-  print(S)
-  M=N+1
   for (i in 1:nRows) {
-    V[i, M] = max( 0, cp * (S[i, M]-K))
+    V[i, nCols] = max( 0, cp * (S[i, nCols]-K))
   }
   print(V)
   # V
   # Step backwards through the tree ----
-  p = (exp(r*dt)-d)/(u-d)
   for (j in (nCols-1):1) {
     for(i in (nCols-j+1):(nCols+j-1)) {
       print((nCols-j+1):(nCols+j-1))
       V[i, j] = disc * (p*V[i-1,j+1] + (1-p)*V[i+1,j+1])
       if(isAmerican) {
+        # if american option, then take the Value at each node as the max of the
+        # value of option or the payoff at that period
         V[i, j] = max(V[i, j], cp * (S[i, j] - K))
       }
     } 
   }
-  V
+  #Returning all the calculated values as a list.
+  list(Type = paste( ifelse(isAmerican, "American", "European"), 
+                     ifelse(isCall, "Call", "Put")),
+       Price = V[nCols,1], S=round(S,2), V=round(V,4))
 }
 
 
-Clewlow3_3(isCall=T, isAmerican=F)
+BinomialTree(isCall=T, isAmerican=F)
 
 # BinomialTree(isCall=T, isAmerican=F)
 # Clewlow2_3n5(isCall=F, isAmerican=T)
