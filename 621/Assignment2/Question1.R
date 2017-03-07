54e90e28f3fa31efac6e1aca4e93f6865df3f8b8
Saeed Rahman <saeed349@gmail.com>
2017-03-06 21:06:41 -0500

    Made the correct multiplicative tree, but have a strong feeling that I dumped the previous algorithm in mistaking it for wrong due to wrong parameters in the model. The other one was visually more pleasing. Have to check it in detail

--- 621/Assignment2/Question1.R
+++ 621/Assignment2/Question1.R
@@ -1,116 +1,61 @@
 library(quantmod)
 library(fOptions)
-#professors implementation
-BinomialTreeOld= function(isCall, isAmerican, K=100, Tm=1, 
-                        S0=100, r=0.06, N=3, u=1.1, d=1/u)
+library(rCharts)
+
+#Binomial Tree using multiplicative method----
+BinomialTree = function(isCall, isAmerican=FALSE, K, Tm, 
+                      S0, r, sig, N,div=0)
 {
   # Precompute constants ----
-  dt = Tm/N # diving the time into partition
-  p = (exp(r*dt)-d)/(u-d) #calculating the probability of up move
-  disc = exp(-r*dt) #discount factor
-  M = N+1 #in r, index starts from 0, whereas state in our tree starts from 0
-  cp = ifelse(isCall, 1, -1) # 1 for CE or -1 for PE 
+  dt = Tm/N
+  nu=r-div-0.5*sig*sig
+  dxu=sqrt(sig*sig*dt+((nu*dt)^2))
+  dxd=-dxu
+  pu=0.5+0.5*(nu*dt/dxu)
+  pd=1-pu
+  disc=exp(-r*dt)
+
+  dpu=disc*pu
+  dpd=disc*pd
+  edxud=exp(dxu-dxd)
+  edxd=exp(dxd)
   
-  # Intialize asset prices  ----
-  # Intitialize an empty array of size 
-  V = S = matrix(0, nrow=M, ncol=M, dimnames=list(
-    paste("State", 1:(N+1),sep=""), paste("T=",0:N,sep="")))
-  # print(V)
-  S[1,1] = S0
-  for (j in 2:M) {
-    S[1, j] = S[1, j-1]*u # multiplying the previous column member in same row to get the up move
-    for(i in 2:j) {
-      S[i, j] = S[i-1, j-1]*d #multiplying d with the diagonal element to get the down move
-    }
-  }
-  S
-  # Intialize option values at maturity ----
-  for (j in 1:M) {
-    V[M-j+1, M] = max( 0, cp * (S[M-j+1, M]-K))
+  #to check if its a call or a put
+  cp = ifelse(isCall, 1, -1) 
+  
+  #setting an initial matrix
+  S = V = matrix(0, nrow=N+1, ncol=1)
+  
+  #setting the last up move of the stock at state N
+  S[1]=S0*exp(N*dxd) 
+  
+  for(i in 1:N+1){
+    S[i]=S[i-1]*edxud
   }
   
-  # Step backwards through the tree ----
-  for (j in (M-1):1) {
-    for (i in 1:j) {
-      V[i, j] = disc * ( p*V[i, j+1] + (1-p)*V[i+1, j+1] )
+  #calculating the payoff at maturity
+  for (j in 1:N+1) {
+    V[j] = max( 0, cp * (S[j]-K))
+  }
+  
+  for (i in seq(from=N, to=1, by=-1)){
+    for( j in 1:i){
+      V[j]=disc*(pu*V[j+1]+pd*V[j])
+      S[j]=S[j]/edxd
       if(isAmerican) {
-        V[i, j] = max(V[i, j], cp * (S[i, j] - K))
+        V[j] = max( V[j], cp * (S[j]-K))
       }
     }
   }
-  V
-  # Return the price ----
-  list(Type = paste( ifelse(isAmerican, "American", "European"), 
-                     ifelse(isCall, "Call", "Put")),
-       Price = V[1,1], S=round(S,2), V=round(V,4))
+  return(V[1])
 }
 
-#inspired from clewlaws trinomial tree
-BinomialTree = function(isCall, isAmerican=FALSE, K=100, Tm=1, 
-                      S0=100, r=0.06, sig=0.2, N=3, u=1.1, d=1/u,div=0)
-{
-  # dt = Tm/N
-  # nu=r-div-0.5*sig*sig 
-  # dxu=sqrt(sig*sig*dt+((nu*dt)^2))
-  # dxd=-dxu
-  # pu=0.5+0.5*(nu*dt/dxu)
-  # pd=1-pu
-  # disc=exp(-r*dt)
-  # 
-  # dpu=disc*pu
-  # dpd=disc*pd
-  # edxud=exp(dxu-dxd)
-  # edxd=exp(dxd)
-  
-  # Precompute constants ----
-  dt = Tm/N # diving the time into partition, to get time for each state
-  disc = exp(-r*dt)
-  p = (exp(r*dt)-d)/(u-d) #calculating the probability of up move
-  nRows = 2*N+1 #number of rows for the matrix
-  nCols = N+1 #number of columns
-  cp = ifelse(isCall, 1, -1) # to check ifts a call or a put
-  
-  # Intialize asset prices  ----
-  # Creating a matrix of nRows*nColumns with zeros and headings 
-  V = S = matrix(0, nrow=nRows, ncol=nCols, dimnames=list(
-    paste("NumUps", N:-N, sep="="), paste("T", 0:N, sep="=")))
-  S[nCols, 1] = S0 # initial stock price
-  
-  # iterating the elements of the matrix in a conical manner starting
-  # from the position of initial stock price
-  # For n=3, S[i,j]= S0, then update the forward diagonal elements 
-  # Code is similar to the one used for trinomial tree
-  for (j in 1:N) {
-    for(i in (nCols-j+1):(nCols+j-1)) {
-      S[i-1, j+1] = S[i, j] * u
-      S[i+1, j+1] = S[i, j] * d
-      # S[i-1, j+1] = S[i, j]*exp(dxu)
-      # S[i+1, j+1] = S[i, j] *exp(dxd)
-    }
-  }
-  for (i in 1:nRows) {
-    V[i, nCols] = max( 0, cp * (S[i, nCols]-K))
-  }
-  # print(S)
-  # V
-  # Step backwards through the tree ----
-  for (j in (nCols-1):1) {
-    for(i in (nCols-j+1):(nCols+j-1)) {
-      V[i, j] = disc * (p*V[i-1,j+1] + (1-p)*V[i+1,j+1])
-      # V[i, j] = disc * (pu*V[i-1,j+1] + pd*V[i+1,j+1])
-      if(isAmerican) {
-        # if american option, then take the Value at each node as the max of the
-        # value of option or the payoff at that period
-        V[i, j] = max(V[i, j], cp * (S[i, j] - K))
-      }
-    } 
-  }
-  #Returning all the calculated values as a list.
-  # list(Type = paste( ifelse(isAmerican, "American", "European"), 
-  #                    ifelse(isCall, "Call", "Put")),
-  #      Price = V[nCols,1], S=round(S,2), V=round(V,4))
-  return(V[nCols,1])
-}
+BinomialTree(isCall=TRUE,K=100,Tm =1 ,S0 =100 ,sig = .2,N =200,r=.06)
+BinomialTree(isCall=FALSE,K=100,Tm =1 ,S0 =100 ,sig = .2,N =200,r=.06)
+BinomialTree(isCall=TRUE,isAmerican = TRUE,K=100,Tm =1 ,S0 =100 ,sig = .2,N =200,r=.06)
+BinomialTree(isCall=FALSE,isAmerican = TRUE,K=100,Tm =1 ,S0 =100 ,sig = .2,N =200,r=.06)
+
+#----
 
 #Black sholes merton pricing function
 BSM<-function(S, K, t, r, sigma,type){
@@ -123,6 +68,8 @@ BSM<-function(S, K, t, r, sigma,type){
   return(result)
 }
 
+
+#Calculating Implied Vol----
 #Implementation of secant method that was used in assignment 1
 Secant <- function(S, K, t, r, type, option_price
                              , x0=0.1, x1=3, tolerance=1e-07, max.iter=10000){
@@ -146,71 +93,8 @@ Secant <- function(S, K, t, r, type, option_price
     return(list(x2,time.taken,count))
 }
 
-
-Secant_Jacob <- function(S, K, t, r, type, option_price
-                                  , x0=0.1, x1=3, tolerance=1e-07, max.iter=10000){
-
-  error=1e-04
-  iter=1
-  volGuess=1.0
-  deltavol=1e-04
-  BSMvolGuess=1000.0
-  volLower=0.0
-  volMid=0.5
-  # startime
-  while(abs(BSMvolGuess)>error){
-    if(is.infinite(volGuess)){
-      print(paste("volGuess_temp=",volGuess_temp,
-                  "BSMvolGuess_temp=",BSMvolGuess_temp,
-                  "volLower_temp=",volLower_temp,
-                  "BSMvolGuess_temp=",BSMvolGuess_temp,
-                  "BSMvolLower_temp=",BSMvolLower_temp))
-    }
-    BSMvolGuess= BSM(S=S,K=K,t=t,r=r,sigma=volGuess,type=type)-option_price
-    # t=volGuess
-    volLower=volGuess - deltavol
-    BSMvolLower= BSM(S=S,K=K,t=t,r=r,sigma=volLower,type=type)-option_price
-    
-    #Testing out by printing----
-    volGuess_temp=volGuess
-    BSMvolGuess_temp=BSMvolGuess
-    volLower_temp=volLower
-    BSMvolGuess_temp=BSMvolGuess
-    BSMvolLower_temp=BSMvolLower
-    #----
-    if(BSMvolGuess==BSMvolLower){
-      print("-------------------SSSSSSSSSSSSSS------")
-      # volLower=volGuess + 1e-1
-      volLower=runif(1, min=0, max=1)
-      volGuess=runif(1, min=0, max=1)
-      BSMvolLower= BSM(S=S,K=K,t=t,r=r,sigma=volLower,type=type)-option_price
-      print(paste("volGuess=",volGuess,
-                  "BSMvolGuess=",BSMvolGuess,
-                  "volLower=",volLower,
-                  "BSMvolGuess=",BSMvolGuess,
-                  "BSMvolLower=",BSMvolLower))
-      print("=======================================")
-    }
-    volGuess=volGuess-BSMvolGuess/((BSMvolGuess-BSMvolLower)/(volGuess-volLower))
-    
-    iter=iter+1
-    if(iter == 5000){
-      return (volGuess)
-    }
-    if(is.na(BSMvolGuess)){
-      # print(paste("volguess previous=","volGuess=",volGuess))
-      # print(BSM(S=S,K=K,t=t,r=r,sigma=volGuess,type=type))
-      # print(paste("BSMvolGuess",BSMvolGuess))
-    }
-    
-  }
-  return (volGuess)
-}
-
-Secant_Jacob(S=100,K=100,t=30/252,r=.05,type='c',option_price = 3.051184)
-
 #Calculating IV on the option chain using secant method
-ImpliedVol_Secantfunction<-function(symbol="AAPL",option_chain=option_chain_csv,rate=.75/100){
+ImpliedVol_Secant<-function(symbol="AAPL",option_chain,rate=.75/100){
   symbol="AAPL"
   stock_df<-as.data.frame(getSymbols(symbol,from = as.Date("2017-01-01"),to=as.Date("2017-02-19"), env = NULL))
 
@@ -227,6 +111,7 @@ ImpliedVol_Secantfunction<-function(symbol="AAPL",option_chain=option_chain_csv,
   for (i in 1:nrow(option_chain)) 
   {
     try({
+      #Myoldmethod----
        secant <- Secant(
        S = as.numeric(tail(stock_df,1)[6]),
        K = as.numeric(option_chain[i,"Strike"]),
@@ -290,61 +175,148 @@ ImpliedVol_Secantfunction<-function(symbol="AAPL",option_chain=option_chain_csv,
   list(option_chain_df,time.taken,iterations)
 }
 
-BinomialTree(isCall=T, isAmerican=F)
+#Jacob chettans secant method----
+Secant_Jacob <- function(S, K, t, r, type, option_price
+                         , x0=0.1, x1=3, tolerance=1e-07, max.iter=10000){
+  
+  error=1e-04
+  iter=1
+  volGuess=1.0
+  deltavol=1e-04
+  BSMvolGuess=1000.0
+  volLower=0.0
+  volMid=0.5
+  # startime
+  while(abs(BSMvolGuess)>error){
+    if(is.infinite(volGuess)){
+      print(paste("volGuess_temp=",volGuess_temp,
+                  "BSMvolGuess_temp=",BSMvolGuess_temp,
+                  "volLower_temp=",volLower_temp,
+                  "BSMvolGuess_temp=",BSMvolGuess_temp,
+                  "BSMvolLower_temp=",BSMvolLower_temp))
+    }
+    BSMvolGuess= BSM(S=S,K=K,t=t,r=r,sigma=volGuess,type=type)-option_price
+    # t=volGuess
+    volLower=volGuess - deltavol
+    BSMvolLower= BSM(S=S,K=K,t=t,r=r,sigma=volLower,type=type)-option_price
+    
+    #Testing out by printing----
+    volGuess_temp=volGuess
+    BSMvolGuess_temp=BSMvolGuess
+    volLower_temp=volLower
+    BSMvolGuess_temp=BSMvolGuess
+    BSMvolLower_temp=BSMvolLower
+    #----
+    if(BSMvolGuess==BSMvolLower){
+      print("-------------------SSSSSSSSSSSSSS------")
+      # volLower=volGuess + 1e-1
+      volLower=runif(1, min=0, max=1)
+      volGuess=runif(1, min=0, max=1)
+      BSMvolLower= BSM(S=S,K=K,t=t,r=r,sigma=volLower,type=type)-option_price
+      print(paste("volGuess=",volGuess,
+                  "BSMvolGuess=",BSMvolGuess,
+                  "volLower=",volLower,
+                  "BSMvolGuess=",BSMvolGuess,
+                  "BSMvolLower=",BSMvolLower))
+      print("=======================================")
+    }
+    volGuess=volGuess-BSMvolGuess/((BSMvolGuess-BSMvolLower)/(volGuess-volLower))
+    
+    iter=iter+1
+    if(iter == 5000){
+      return (volGuess)
+    }
+    if(is.na(BSMvolGuess)){
+      # print(paste("volguess previous=","volGuess=",volGuess))
+      # print(BSM(S=S,K=K,t=t,r=r,sigma=volGuess,type=type))
+      # print(paste("BSMvolGuess",BSMvolGuess))
+    }
+    
+  }
+  return (volGuess)
+}
+
+Secant_Jacob(S=100,K=100,t=30/252,r=.05,type='c',option_price = 3.051184)
+
+#-----
 
 
 #Importing and using the option data ----
 #Importing the option chain downloaded from yahoo finance
-option_chain_csv <- read.csv(file="call.csv",header=TRUE, sep=",")
+option_chain_csv <- read.csv(file="put.csv",header=TRUE, sep=",")
 #call.csv and put.csv are available in the project folder
-option_chain_csv$days_till_expiry <- as.Date(option_chain_csv$Expiry,"%m/%d/%Y")-as.Date("2017-02-19")
+#IMP to remove #both call and put have different format in the csv
+#Call
+# option_chain_csv$days_till_expiry <- as.Date(option_chain_csv$Expiry,"%m/%d/%Y")-as.Date("2017-02-19")
+#Put
+option_chain_csv$days_till_expiry <- as.Date(option_chain_csv$Expiry,"%Y/%m/%d")-as.Date("2017-02-19")
 #Calculating the days till expiry
-
 option_chain_csv$premium<-(option_chain_csv$Bid+option_chain_csv$Ask)/2
 
 head(option_chain_csv)
 
 
 #----
+
 jacob_secant
 foption_secant
 assignment1_secant
 
-
-df=ImpliedVol_Secantfunction()
+df=0
+df=ImpliedVol_Secant(option_chain = option_chain_csv)
 df=as.data.frame(df[1])
 # option.df=complete.cases(df[,Implied_Volatility])
 options.df=df[complete.cases(df$Implied_Volatility),]
 
-BSM(S=100,K=100,t=30/252,r=.75/100,sigma=.2,type="c")
-
-bin_price=BinomialTree(isCall=TRUE,K=100,Tm =30/252 ,S0 =100 ,sig = .2,N =200,r=.06,u=1.01)
-# BinomialTree(isCall=T, isAmerican=F)
-# Clewlow2_3n5(isCall=F, isAmerican=T)
-
-#
 QuestionC<-function(options.df){
   eur_binomial <-{}
   eur_bsm <-{}
   ame_binomial <-{}
-  ame_bsm <-{}
-  
+  stock_df<-as.data.frame(getSymbols("AAPL",from = as.Date("2017-01-01"),
+                                     to=as.Date("2017-02-19"), env = NULL))
   for (i in 1:nrow(options.df)){
-    stock_df<-as.data.frame(getSymbols("AAPL",from = as.Date("2017-01-01"),
-                                       to=as.Date("2017-02-19"), env = NULL))
     eur_binomial <- append(eur_binomial,BinomialTree(
                        isCall = as.logical(options.df[i,"Type"]=="Call"),
                            K  = as.numeric(options.df[i,"Strike"]),
-                           Tm = as.numeric(options.df[i,"Days_till_Expiry"]),
+                           Tm = as.numeric(options.df[i,"Days_till_Expiry"])/252,
                            S0 = as.numeric(tail(stock_df,1)[6]),
                           sig = as.numeric(options.df[i,"Implied_Volatility"]),
                            r  = .75/100,
                             N = 200))
-    # print(eur_binomial)
+    ame_binomial <- append(ame_binomial,BinomialTree(
+                    isAmerican = TRUE, 
+                        isCall = as.logical(options.df[i,"Type"]=="Call"),
+                            K  = as.numeric(options.df[i,"Strike"]),
+                            Tm = as.numeric(options.df[i,"Days_till_Expiry"])/252,
+                            S0 = as.numeric(tail(stock_df,1)[6]),
+                           sig = as.numeric(options.df[i,"Implied_Volatility"]),
+                            r  = .75/100,
+                             N = 200))
+    eur_bsm <- append(eur_bsm,BSM(
+                      type = ifelse(options.df[i,"Type"]=="Call",'c','p'),
+                        K  = as.numeric(options.df[i,"Strike"]),
+                        t  = as.numeric(options.df[i,"Days_till_Expiry"])/252,
+                        S  = as.numeric(tail(stock_df,1)[6]),
+                     sigma = as.numeric(options.df[i,"Implied_Volatility"]),
+                        r  = .75/100))
+    # BSM(S=100,K=100,t=30/252,r=.75/100,sigma=.2,type="c")
                            
   }
-  return(eur_binomial)
+  options.df$BinomialEuropean <- eur_binomial
+  options.df$BinomialAmerican <- ame_binomial
+  options.df$BlackSholesMertonEuropean <- eur_bsm
+  return(options.df)
   
 }
   
 df <- QuestionC(options.df)
+df[1]
+plot(df$Strike,df$BinomialAmerican)
+
+# h1=hPlot(Implied_Volatility ~ strike, data =options.data, type ='line',
+#          group = 'Days_till_Expiry')
+# # h1$legend(enabled=FALSE)
+# h1$chart(zoomType="xy")
+# # h1
+# # h1$show('iframesrc',cdn=TRUE)
+# h1$print(include_assets = TRUE)
