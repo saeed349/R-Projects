4c031fc065a6d7d426cc31f7d28071034d5ba59f
DESKTOP-P4R6VD1\saeed <saeed349@gmail.com>
2017-03-21 11:26:31 -0400

    Vaguely completed question 1

--- 621/Assignment3/Question1.R
+++ 621/Assignment3/Question1.R
@@ -1,5 +1,6 @@
 # Explicit Finite Difference method----
-Explicit <- function(isAmerican, isCall, K, Tm,S0, r, sig, N, div, dx){
+  # Explicit Implementation----
+Explicit <- function(isAmerican, isCall, K, Tm,S0, r, sig, N, div, dx,returnGreeks=FALSE){
   # Finite Difference Method: i times, 2*i+1 final nodes
   # Precompute constants ----
   dt = Tm/N
@@ -53,21 +54,33 @@ Explicit <- function(isAmerican, isCall, K, Tm,S0, r, sig, N, div, dx){
         V[j, i] = max(V[j, i], cp * (S[j, lastCol] - K))
       }
   }
+  # Calculating the Greeks and returning ----
+  if(returnGreeks && isCall){
+    delta = (V[middleRow-1,firstCol+1]-V[middleRow+1,firstCol+1])/
+      (S[middleRow-1,firstCol+1]-S[middleRow+1,firstCol+1])
+    delta1 =(V[middleRow-1,firstCol+1]-V[middleRow,firstCol+1])/
+      (S[middleRow-1,firstCol+1]-S[middleRow,firstCol+1])
+    delta2 =(V[middleRow,firstCol+1]-V[middleRow+1,firstCol+1])/
+      (S[middleRow,firstCol+1]-S[middleRow+1,firstCol+1])
+    gamma = 2*(delta1-delta2)/((S[middleRow-1,firstCol+1]-S[middleRow+1,firstCol+1]))
+    theta =(V[middleRow,firstCol+1]-V[middleRow,firstCol])/dt
+    return(list(Price=V[middleRow,firstCol],Delta=delta,Gamma=gamma,Theta=theta))
+  }
   # Return the price ----
   # list(Type = paste( "European", ifelse(isCall, "Call", "Put")),
   #      Price = V[middleRow,firstCol], Probs=round(c(pu=pu, pm=pm, pd=pd),4), S=round(S,2), V=round(V,4))
   return(V[middleRow,firstCol])
 }
   #Implementation of Explicit----
-Explicit(isAmerican=FALSE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2)
+Explicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2,returnGreeks = TRUE)
 #European put value is =5.6217
 #European call value is =8.5455 (verified)
 #Ameican put value = 6.0058 (verified)
 
+
 #Implicit method----
+  # Implicit Implementation----
 Implicit = function(isAmerican,isCall, K, Tm,S0, r, sig, N, div, dx){
-# (isCall, K=100, Tm=1, 
-#                        S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2)
   # Implicit Finite Difference Method: i times, 2*i+1 final nodes
   # Precompute constants ----
   dt = Tm/N
@@ -133,7 +146,7 @@ Implicit = function(isAmerican,isCall, K, Tm,S0, r, sig, N, div, dx){
   # list(Type = paste( "American", ifelse(isCall, "Call", "Put")),Price = V[middleRow,firstCol],
   #      Probs=round(c(pu=pu, pm=pm, pd=pd),middleRow), pmp=round(pmp,4), pp=round(pp,4),
   #      S=round(S,2), V=round(V,middleRow))
-  return(V[middleRow,firstCol])
+  return(list(Price=V[middleRow,firstCol],Probs=round(c(pu=pu, pm=pm, pd=pd),middleRow)))
 }
   # Solving the tridiagonal matrix---- 
 solveImplicitTridiagonal=function(V, pu, pm, pd, lambdaL, lambdaU, colI)
@@ -166,13 +179,12 @@ solveImplicitTridiagonal=function(V, pu, pm, pd, lambdaL, lambdaU, colI)
   list(V=V, pmp=pmp, pp=pp)
 }
   # Testing the implicit method ----
-Implicit(isAmerican=TRUE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2)
+x=Implicit(isAmerican=TRUE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.2, N=3, div=0.03, dx=0.2)
 #Implicit American Put = 4.9221 (verified from textbook)
 #Implicit American Call=7.5608 (not verified)
 #Implicit European Call=7.5608 (not verified)
 #Implicit European Put =4.768 (not verified)
 #CHECK FOR EUROPEAN as well and verify. Also check for American Call and verify
-
 
 #Crank Nicholson Method----
 CrankNicholson = function(isAmerican, isCall, K, Tm,S0, r, sig, N, div, dx){
@@ -269,12 +281,14 @@ solveCrankNicholsonTridiagonal=function(V, pu, pm, pd, lambdaL, lambdaU, colI)
 
 
   #Implementation of Crank Nicholson----
-CrankNicholson(isAmerican=TRUE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, 
-               sig=0.2, N=3, div=0.03, dx=0.2)
+CrankNicholson(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, 
+               sig=0.25, N=1128, div=0.03, dx=0.2)
 
 
 #American call = 8.0301 (not verified)
 #American put  = 5.4184 (verified)
+
+
 
 
 
@@ -293,23 +307,139 @@ BSM<-function(S, K, t, r, sigma,type){
 }
 BSM(S=100,K=100,t=1,r=.06,sigma=.2,type="c")
 
-#Question1.C----
-# No clue how to do this, the first thing 
-QuestionC<-function(N,sig,Tm,nsd,error){
+
+# Question1.C----
+  # Carried out an implementation from clewlow pg 65---- 
+QuestionC<-function(N=3,sig,Tm,nsd,error){
   repeat{
     dt=Tm/N
     # Nj=((nsd*sig*sqrt(Tm)) / (2*(error-(Tm/N))) ) - .5 # Doesnt work
     Nj=(sqrt(N)*nsd)/(2*sqrt(3)) -.5
     dx=(nsd*sig*sqrt(Tm))/(2*Nj+1)
-    print(paste(dt,Nj,dx))
+    # print(paste(dt,Nj,dx))
     N=N+1
-    if(((dx*dx)+dt)<=.001){
+    if(((dx*dx)+dt)<=error){
       print(paste("ANSWER:",dt,Nj,dx,N))
       return(list(dt,Nj,dx,N))
       break
     }
   }
 }
+  # Testing it for the above conditions ----
 c=QuestionC(N=3,sig=.2,Tm=1,nsd=6,error = .001)
-
 Implicit(isAmerican=FALSE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.2, N=1121, div=0.0, dx=0.0103509833901353)
+
+
+
+
+# Question1.D ----
+
+Explicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=0.25, N=1121, div=0.03, dx=0.0103509833901353)
+# 11.01241
+Explicit(isAmerican=FALSE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.25, N=1121, div=0.03, dx=0.0103509833901353)
+# 8.144172
+Implicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=0.25, N=1121, div=0.03, dx=0.0103509833901353)
+# 11.00983
+Implicit(isAmerican=FALSE,isCall=FALSE, K=100, Tm=1, S0=100, r=0.06, sig=0.25, N=1121, div=0.03, dx=0.0103509833901353)
+# 8.141817
+  # Verify the above answers, Also I have ASSUMED that dx is obtained from the above
+  # Also verify which N to use, because in the problem below I am getting different results
+
+
+# QuestionE ----
+print(QuestionC(sig=.25,Tm=1,nsd=6,error = .001))
+
+
+
+# QuestionF ----
+library(reshape)
+library(ggplot2)
+sig.list = seq(.05,.6,.05)
+pu={}
+pm={}
+pd={}
+for(sig in sig.list){
+  implicit=Implicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100,
+                    r=0.06, sig=sig, N=100, div=0.03, dx=0.0103509833901353)
+  pu=append(pu,implicit$Probs[1])
+  pm=append(pm,implicit$Probs[2])
+  pd=append(pd,implicit$Probs[3])
+  print(sig)
+}
+probability=data.frame(sig.list,pu,pm,pd)
+names(probability)=c('Sigma','pu','pm','pd')
+melted_prob=melt(probability,id.vars ="Sigma")
+ggplot(data = melted_prob, aes(x=Sigma, y=value)) + geom_line(aes(colour=variable))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# QuestionF Proper Implementation ----
+pu = function(sig, dt=1/3, dx=0.2, r=0.06, div=0.03, nu=r-div-0.5*sig^2) {
+  0.5 * dt * ( (sig/dx)^2 + nu/dx )
+}
+
+pm =  function(sig, dt=1/3, dx=0.2, r=0.06, div=0.03, nu=r-div-0.5*sig^2) {
+  1.0 - dt *   (sig/dx)^2 - r*dt 
+}
+pd =  function(sig, dt=1/3, dx=0.2, r=0.06, div=0.03, nu=r-div-0.5*sig^2) {
+  0.5 * dt * ( (sig/dx)^2 - nu/dx)
+}
+
+x = seq(from=0.01, to=0.5, by=0.01)
+ux = pu(x)
+mx = pm(x)
+dx = pd(x)
+theTitle = expression(paste("Figure 3.11: Explicit Finite Difference Probabilities", 
+                            " as a Function of ", sigma, sep=""))
+xlab1 = expression(paste("Volatility (", sigma, ")", sep=""))
+xlab2 = expression(paste("K = 100, T = 1.0, S =100, r = 6%, ", delta, " = 3%"))
+plot(x, ux, ylim=c(-1,1), type="n", xlab="")
+title(theTitle, cex=3)
+mtext(text=xlab1, side=1, line=2, cex=1.25)
+mtext(text=xlab2, side=1, line=3, cex=1.25)
+lines(x, ux, lwd=2, col=2)
+lines(x, mx, lwd=2, col=3)
+lines(x, dx, lwd=2, col=4)
+abline(h=0)
+legend(0.4, 0.5, legend=c('pu', 'pm', 'pd'), col=2:4, lty=1, lwd=3)
+
+
+# QuestionH ----
+sigma=.25
+explicit=Explicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=0.25, N=3, div=0.03, dx=0.2,returnGreeks = TRUE)
+delta=explicit$Delta
+gamma=explicit$Gamma
+theta=explicit$Theta
+dsigma = .001* sigma
+Vega = (Explicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=sigma+dsigma, N=3, div=0.03, dx=0.2) 
+-Explicit(isAmerican=FALSE,isCall=TRUE, K=100, Tm=1, S0=100, r=0.06, sig=sigma-dsigma, N=3, div=0.03, dx=0.2))/(2*dsigma)
+  
+  # Delta = 0.58179
+  # Gamma = 0.0172
+  # Theta = -6.6315
+  # Vega  = 43.1158
+
+  #Made an assumption in the cloumn index in the delta calculations
+
+
+
+  
+  
+  
+  
+  
