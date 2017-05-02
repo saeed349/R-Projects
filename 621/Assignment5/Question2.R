fab3f75ea88122274104441a35e9a79f50c16521
DESKTOP-P4R6VD1\saeed <saeed349@gmail.com>
2017-05-02 01:33:35 -0400

    Update: MC is taking more than 30 minutes to run, yet to verify the answer.

--- 621/Assignment5/Question2.R
+++ 621/Assignment5/Question2.R
@@ -18,15 +18,15 @@ for(i in 1:10000)
   X2 = rnorm(1) 
   
   Y[i]=X1
-  Z[i] = 0.8*X1+sqrt(1-0.4)*X2 
+  Z[i] = 0.8*X1+sqrt(1-0.8^2)*X2 
 }
 
 
 #Monte Carlo Method----
 
 set.seed(07630)
-nIterations = 10000  #should be high 
-nSteps = 30
+nIterations =  100000  #should be high 
+nSteps = 1000
 mu = 0.02
 dt = 1/nSteps
 sigma = 0.03
@@ -47,26 +47,29 @@ xi = 0.61
 # S = matrix(0, nrow=nSteps+1, ncol=nIterations)
 payoff_sum=0
 for( j in 1:nIterations) {
-  S[1,j] = S0
   W1=W2={}
   vol_path=v_0
   vmax=0
   spot_path=S_0
+  W1=W2=0
   for( i in 1:nSteps){
-    X1 = rnorm(1) 
-    X2 = rnorm(1) 
-    W1[i]=X1
-    W2[i] = 0.8*X1+sqrt(1-0.4)*X2 
+    
+    X1 = rnorm(1,0,1) 
+    X2 = rnorm(1,0,1) 
+    W1=X1
+    W2= rho*X1+sqrt(1-rho^2)*X2
     
     vmax= max(vol_path,0)
-    
-    vol_path=vol_path+kappa*T*(theta-vmax)+xi*sqrt(vmax*T)*W1[i] #W1[i-1]
+    # print(paste("vol_path Before=",vol_path))
+    vol_path=vol_path+kappa*T*(theta-vmax)+xi*sqrt(vmax*T)*W1 #W1[i-1]
     # print(paste("Vol=",vol_path))
-    
-    spot_path=spot_path* exp((r - 0.5*vmax)*T +sqrt(vmax*T)*W2[i])
+    # print(paste("vol_path After=",vol_path))
+    spot_path=spot_path* exp((r - 0.5*vmax)*T +sqrt(vmax*T)*W2)
     # print(paste("Spot=",spot_path))
+
   }
   payoff_sum=payoff_sum+max(spot_path-K,0)
+  print(j)
 }
 option_price=(payoff_sum/nIterations)*exp(-r*T)
 print(option_price)
